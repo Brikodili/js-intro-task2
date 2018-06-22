0538f33571aa43dc070b2c7928332df15f62c3b4
anna <brikodili.troyan@gmail.com>
2018-06-22 14:06:00 +0300

    update input data

--- main.js
+++ main.js
@@ -1,24 +1,15 @@
 "use strict";
 
-var a = +prompt("Введите a", "");
-var b = +prompt("Введите b", "");
-var c = +prompt("Введите c", "");
-
-function calcD() {
-    var d;
-    d = (b*b - 4*a*c);
-    if(d >= 0){
-        var res1 = (-b - d/d)/(2*a);
-        var res2 = (-b + d/d)/(2*a);
-        alert(res1);
-        alert(res2);
+function calcD(a, b, c) {
+    var d = (b*b - 4*a*c);
+    var x1 = (-b - (d/d))/(2*a);
+    var x2 = (-b + (d/d))/(2*a);
+    if (d > 0){
+        console.log(x1, x2);
     }
     else {
-        alert("D < 0");
+        console.log("d < 0");
     }
 }
-
-calcD();
-
-
+calcD(1, 2, 1);
 
