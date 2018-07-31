8910b84ca6333347ef320851d2858c85e6ed0179
Anna Brikodili <brikodili.troyan@gmail.com>
2018-07-31 12:53:48 +0300

    refactoring

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
 "use strict";
 
 function calcD(a, b, c) {
-    var d = (b*b - 4*a*c);
-    var x1 = (-b - (d/d))/(2*a);
-    var x2 = (-b + (d/d))/(2*a);
-    if (d > 0){
+    var d, x1, x2;
+
+    d = (b * b - 4 * a * c);
+    x1 = (-b - (d / d)) / (2 * a);
+    x2 = (-b + (d / d)) / (2 * a);
+
+    if (d > 0) {
         console.log(x1, x2);
-    }
-    else {
+    } else {
         console.log("d < 0");
     }
 }
